2f96c63435b71cd525e951c62a171f6c1c92e26d
AIfred-Jiang <alfredj14@nycstudents.net>
2025-02-14 11:08:41 -0500

    committttt

--- guessnumber.py
+++ guessnumber.py
@@ -1,5 +1,4 @@
 
-
 import random
 
 
@@ -9,5 +8,18 @@ import random
 
 def gen(min_value, max_value):
     return random.randint(min_value, max_value)
+random_number = gen(1, 1000)
 
-min_value, max_value = int(input("Guess a number from 1 to 1000"))
+while True:
+    x = int(input("Guess a number from 1 to 1000")) 
+    if x == random_number:
+        print("Correct!")
+        break
+    elif x > random_number:
+        print("The number is less than your input, please try again.")
+    else:  
+        print("The number is greater than your input, please try again.")
+
+        guess_history = []
+
+    
